4a83c87e922bf11d05b474599d142d3a8a3006ac
Tiago Dinis <tiagodinis33@gmail.com>
2022-02-08 12:59:01 +0000

    Add double fault interrupt handler

--- src/idt.rs
+++ src/idt.rs
@@ -9,6 +9,7 @@ lazy_static! {
         idt.breakpoint.set_handler_fn(breakpoint_handler);
         idt.page_fault.set_handler_fn(pagefault_handler);
         idt.general_protection_fault.set_handler_fn(general_protection_fault_handler);
+        idt.double_fault.set_handler_fn(doublefault_handler);
         idt
     };
 }
@@ -25,6 +26,12 @@ extern "x86-interrupt" fn pagefault_handler(
     stack_frame: InterruptStackFrame, error_code: PageFaultErrorCode)
 {
     panic!("EXCEPTION: Page Fault\n{:#?}\nErr: {:#?}", stack_frame, error_code);
+}
+
+extern "x86-interrupt" fn doublefault_handler(
+    stack_frame: InterruptStackFrame, error_code: u64) -> !
+{
+    panic!("EXCEPTION: Double Fault\n{:#?}\nErr: {}", stack_frame, error_code);
 }
 
 
