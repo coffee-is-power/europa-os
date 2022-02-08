95891ba4da1cf54fb0567fb1fc453b42901dd31d
Tiago Dinis <tiagodinis33@gmail.com>
2022-02-08 12:14:03 +0000

    Add page fault and GP fault exceptions

--- src/idt.rs
+++ src/idt.rs
@@ -1,5 +1,5 @@
 
-use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
+use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame, PageFaultErrorCode};
 use crate::println;
 use lazy_static::lazy_static;
 
@@ -7,6 +7,8 @@ lazy_static! {
     static ref IDT: InterruptDescriptorTable = {
         let mut idt = InterruptDescriptorTable::new();
         idt.breakpoint.set_handler_fn(breakpoint_handler);
+        idt.page_fault.set_handler_fn(pagefault_handler);
+        idt.general_protection_fault.set_handler_fn(general_protection_fault_handler);
         idt
     };
 }
@@ -17,4 +19,17 @@ extern "x86-interrupt" fn breakpoint_handler(
     stack_frame: InterruptStackFrame)
 {
     println!("EXCEPTION: BREAKPOINT\n{:#?}", stack_frame);
+}
+
+extern "x86-interrupt" fn pagefault_handler(
+    stack_frame: InterruptStackFrame, error_code: PageFaultErrorCode)
+{
+    panic!("EXCEPTION: Page Fault\n{:#?}\nErr: {:#?}", stack_frame, error_code);
+}
+
+
+extern "x86-interrupt" fn general_protection_fault_handler(
+    stack_frame: InterruptStackFrame, err: u64)
+{
+    panic!("EXCEPTION: GP fault\n{:#?}\n Err: {}", stack_frame, err);
 }
