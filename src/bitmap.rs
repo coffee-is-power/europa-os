9953b96325551647426b4acecf44193a1ec321b9
Tiago Dinis <tiagodinis33@gmail.com>
2022-03-08 23:31:46 +0000

    Fixed some bugs in bitmap

--- src/bitmap.rs
+++ src/bitmap.rs
@@ -1,8 +1,6 @@
-use core::ops::{Index};
-
-struct BitMap {
-    bitmap: &'static mut [u8],
-    size: usize,
+pub struct BitMap {
+    pub(crate) bitmap: *mut u8,
+    pub(crate) size: usize,
 }
 
 impl BitMap {
@@ -11,11 +9,14 @@ impl BitMap {
             return Err(())
         }
         let byte_index = i / 8;
-        let bit_index = i & 8;
+        let bit_index = i % 8;
         let bit_indexer = 0b10000000 >> bit_index;
-        if (self.bitmap[byte_index] & bit_indexer) > 0 {
-            return Ok(true)
+        unsafe {
+            if (*(self.bitmap.offset(byte_index as isize)) & bit_indexer) > 0 {
+                return Ok(true)
+            }
         }
+
         Ok(false)
     }
     pub fn set(&mut self, i: usize, value: bool) -> Result<bool, ()> {
@@ -24,20 +25,16 @@ impl BitMap {
         }
 
         let byte_index = i / 8;
-        let bit_index = i & 8;
+        let bit_index = i % 8;
         let bit_indexer = 0b10000000 >> bit_index;
-        self.bitmap[byte_index] &= !bit_indexer;
+        unsafe {
+            *(self.bitmap.offset(byte_index as isize)) &= !bit_indexer;
+        }
         if value {
-            self.bitmap[byte_index] |= bit_indexer;
+            unsafe {
+                *(self.bitmap.offset(byte_index as isize)) |= bit_indexer;
+            }
         }
         return Ok(value)
-    }
-}
-
-impl Index<usize> for BitMap {
-    type Output = bool;
-
-    fn index(&self, index: usize) -> &Self::Output {
-        &self.get(index).unwrap()
     }
 }
