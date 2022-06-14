b94e5441d5cfa60e88392263e2a10d3de3fce134
Tiago Dinis <tiagodinis33@gmail.com>
2022-06-14 18:19:51 +0100

    fix memory/mod.rs

--- src/memory/mod.rs
+++ src/memory/mod.rs
@@ -2,7 +2,6 @@ use alloc::alloc::alloc;
 use core::alloc::Layout;
 use x86_64::structures::paging::{PageTable, PageTableFlags};
 use x86_64::{PhysAddr, VirtAddr};
-use crate::get_current_page_table;
 pub mod paging;
 mod bitmap;
 mod allocator;
@@ -19,7 +18,7 @@ pub unsafe fn map_mem(virtual_memory: VirtAddr, physical_memory: PhysAddr) {
     let l3_index = virtual_memory.p3_index();
     let l2_index = virtual_memory.p2_index();
     let l1_index = virtual_memory.p1_index();
-    let l4 = get_current_page_table();
+    let l4 = paging::get_current_page_table();
     let l3 = &mut l4[l4_index];
     if !l3.flags().contains(PageTableFlags::PRESENT) {
         l3.set_addr(alloc_page(), PageTableFlags::PRESENT | PageTableFlags::WRITABLE);
